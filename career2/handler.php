977d36c8326956b0ee3ccc8aa29179145a98f0c8
shashankts <42305065+shashankts@users.noreply.github.com>
2020-11-14 15:17:53 +0530

    Add files via upload

---
+++ career2/handler.php
@@ -0,0 +1,33 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/*
+Tested working with PHP5.4 and above (including PHP 7 )
+
+ */
+require_once './vendor/autoload.php';
+
+use FormGuide\Handlx\FormHandler;
+
+
+$pp = new FormHandler(); 
+
+$validator = $pp->getValidator();
+$validator->fields(['fName','lName','Email'])->areRequired()->maxLength(50);
+$validator->field('Email')->isEmail();
+$validator->field('phoneNumber')->areRequired()->minLength(10);
+$validator->field('Message')->maxLength(6000);
+
+
+
+$pp->attachFiles(['image']);
+
+
+$pp->sendEmailTo('shashank@solosos.com'); 
+$pp->sendEmailTo('recruit@solosos.com');
+	$pp->sendEmailTo('pooja@solosos.com');
+	$pp->sendEmailTo('dianna@solosos.com');
+	$pp->sendEmailTo('soumya@solosos.com');
+
+echo $pp->process($_POST);
